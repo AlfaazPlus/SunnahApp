d80e68ed8da482d401d22df60add421b3d1a1127
Faisal Khan <faisalknx@gmail.com>
2025-03-30 03:14:43 +0530

    alpha 8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.alfaazplus.sunnah"
         minSdk = 21
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.1.0-alpha.7"
+        versionCode = 8
+        versionName = "0.1.0-alpha.8"
 
         setProperty("archivesBaseName", versionName)
 
