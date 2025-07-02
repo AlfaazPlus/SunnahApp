393b9bfb9021fdf984dcefaa8e31da7a2291267a
Faisal Khan <faisalknx@gmail.com>
2025-07-02 08:32:44 +0530

    alpha 14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.alfaazplus.sunnah"
         minSdk = 21
         targetSdk = 35
-        versionCode = 13
-        versionName = "0.1.0-alpha.13"
+        versionCode = 14
+        versionName = "0.1.0-alpha.14"
 
         setProperty("archivesBaseName", versionName)
 
