dd49266bb92fb7d7f9a6bcc078dff85a0cb813e6
Faisal Khan <faisalknx@gmail.com>
2026-03-20 03:54:06 +0530

    bump version to `0.1.3`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.alfaazplus.sunnah"
         minSdk = 23
         targetSdk = 36
-        versionCode = 17
-        versionName = "0.1.2"
+        versionCode = 18
+        versionName = "0.1.3"
 
         resValue("string", "app_name_generated", "SunnahApp")
         resValue("string", "cleartextTrafficPermitted", "false")
